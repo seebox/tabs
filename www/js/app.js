dff5a5059e500af0d0f0bf0dc3f85f331dcfb87f
seebox <sxh1982@126.com>
2016-08-09 10:49:02 +0800

    dd

---
+++ www/js/app.js
@@ -0,0 +1,156 @@
+// Ionic Starter App
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform, $rootScope, $state, $location) {
+  $ionicPlatform.ready(function() {
+    $rootScope.$host = '';
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+      $rootScope.$host = 'http://218.249.66.27:8888';
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+    if (window.localStorage.token) {
+      $rootScope.$token = window.localStorage.token;
+    } else {
+      //$state.go("login",{},{reload:true});
+      window.location.href = 'login.html';
+    }
+
+    //启动极光推送服务
+    window.plugins.jPushPlugin.init();
+
+                  //  window.cordova.plugins.jPushPlugin.setDebugMode(true);
+                  //  window.cordova.plugins.jPushPlugin.setStatisticsOpen(true);
+
+
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+  $ionicConfigProvider.platform.ios.tabs.style('standard');
+  $ionicConfigProvider.platform.ios.tabs.position('bottom');
+  $ionicConfigProvider.platform.android.tabs.style('standard');
+  $ionicConfigProvider.platform.android.tabs.position('standard');
+  $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+  $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+  $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+  $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+  $ionicConfigProvider.platform.ios.views.transition('ios');
+  $ionicConfigProvider.platform.android.views.transition('android');
+
+  $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+    })
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.msg', {
+      url: '/msg',
+      views: {
+        'tab-msg': {
+          templateUrl: 'templates/tab-msg.html',
+          controller: 'MsgCtrl'
+        }
+      }
+    })
+    .state('tab.anno', {
+      url: '/anno',
+      views: {
+        'tab-anno': {
+          templateUrl: 'templates/tab-anno.html',
+          controller: 'AnnoCtrl'
+        }
+      }
+    })
+    .state('tab.anno-detail', {
+      url: '/anno/:id',
+      views: {
+        'tab-anno': {
+          templateUrl: 'templates/anno-detail.html',
+          controller: 'AnnoDetailCtrl'
+        }
+      }
+    })
+    .state('tab.work', {
+      url: '/work',
+      views: {
+        'tab-work': {
+          templateUrl: 'templates/tab-work.html',
+          controller: 'WorkCtrl'
+        }
+      }
+    })
+
+  .state('tab.work-list', {
+      url: '/work/:en',
+      views: {
+        'tab-work': {
+          templateUrl: 'templates/work-list.html',
+          controller: 'WorkListCtrl'
+        }
+      }
+    })
+    .state('tab.work-detail', {
+      url: '/work/detail/:id',
+      views: {
+        'tab-work': {
+          templateUrl: 'templates/work-detail.html',
+          controller: 'WorkDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/msg');
+
+})
+
+.config(['$httpProvider', function($httpProvider) {
+  function authInterceptor($location, $injector,$q) {
+    var interceptor = {
+      'request': function(config) {
+        config.headers = config.headers || {};
+
+        if (window.localStorage.token) {
+          config.headers.Authorization = window.localStorage.token;
+        }
+        return config;
+      },
+      'responseError': function(response) {
+        if (response.status === 401) {
+            //$injector.get('$state').go("login",{},{reload:true});
+            window.location.href = 'login.html';
+          }
+        return $q.reject(response);
+      }
+    };
+    return interceptor;
+  }
+  authInterceptor.$inject = ['$location', '$injector','$q'];
+  $httpProvider.interceptors.push(authInterceptor);
+
+}]);
---
+++ www/js/controllers.js
@@ -0,0 +1,186 @@
+angular.module('starter.controllers', [])
+
+.controller('LoginCtrl', function($scope, $http, $rootScope, $state,$ionicLoading,$ionicBackdrop) {
+  $scope.isAuth = window.localStorage.username;
+  $scope.loginData = {};
+  $scope.doLogin = function(username, passowrd) {
+
+    var account = $scope.loginData.username || username;
+    var password = $scope.loginData.password || passowrd;
+
+    $http.post($rootScope.$host+'/gettoken', {
+      account: account,
+      password: password
+    }).success(function(data) {
+
+      if (data.res_code == 100) {
+        window.localStorage.token = data.user_info.token;
+        window.localStorage.username = account;
+        window.localStorage.password = password;
+        $rootScope.$token = data.user_info.token;
+        $ionicLoading.hide();
+         $state.go("tab.work", {}, {location:false,
+           reload: true
+         });
+      }
+    });
+  };
+
+  if (false && window.localStorage.username && window.localStorage.password) {
+    $ionicLoading.show({
+     template: '正在加载...'
+    });
+    $scope.doLogin(window.localStorage.username,
+      window.localStorage.password);
+  }
+
+})
+
+.controller('MsgCtrl', function($scope) {
+  var i = 0;
+})
+
+.controller('AnnoCtrl', function($scope, $http, $rootScope) {
+  var count = 10,
+    pagenum = 1,
+    total = 10;
+  $scope.playlists = [];
+  $scope.loadAnnoData = function(isRefresh) {
+    $http.get($rootScope.$host + '/lua-api/v1/anno/open', {
+      params: {
+        count: count,
+        pagenum: pagenum
+      }
+    }).success(function(data) {
+      if (isRefresh) {
+        $scope.playlists = data.cards;
+      } else {
+        $scope.playlists = $scope.playlists.concat(data.cards);
+      }
+      total = data.total;
+      pagenum++;
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  }
+
+  $scope.moreDataCanBeLoaded = function() {
+    return $scope.playlists.length < total;
+  }
+  $scope.doRefresh = function() {
+    count = 10;
+    pagenum = 1;
+    total = 10;
+    $scope.loadAnnoData(true);
+  }
+
+  $scope.active = function(item) {
+    $rootScope.title = item.name;
+  }
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('AnnoDetailCtrl', function($scope, $http, $rootScope, $stateParams,$ionicModal) {
+  $http.get($rootScope.$host + '/lua-api/v1/card/get?id=' + $stateParams.id).success(function(data) {
+    if (data.error) {
+
+    } else {
+      $scope.annodata = data;
+    }
+
+  });
+
+  $ionicModal.fromTemplateUrl('templates/pic-preview.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    backdropClickToClose: true
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.preview = function(atm) {
+    if(atm.type == 'pdf'){
+      if (atm.pagecount) {
+  			var urls = [];
+  			for (var i = 1; i <= atm.pagecount; i++) {
+  				urls.push($rootScope.$host +'/wx/pdf/page/' + atm.id + '/' + i.toString()+'.png?token='+$rootScope.$token);
+  			}
+        $scope.pics = urls;
+	  	}
+    }
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //当我们用到模型时，清除它！
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+})
+
+.controller('WorkCtrl', function($scope, $http, $rootScope, Chats) {
+
+  $http.get($rootScope.$host + '/lua-api/v1/app/commonlist').success(function(data) {
+    $scope.playlists = data;
+  });
+
+  $scope.active = function(item) {
+    $rootScope.workTitle = item.name;
+  }
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('WorkListCtrl', function($scope, $http, $rootScope, $stateParams) {
+  var count = 10,
+    pagenum = 1,
+    total = 10;
+  $scope.playlists = [];
+  $scope.loadWorkData = function(isRefresh) {
+    $http.get($rootScope.$host + '/lua-api/v1/workapp/stream/page', {
+      params: {
+        formname: $stateParams.en,
+        count: count,
+        pagenum: pagenum
+      }
+    }).success(function(data) {
+      if (isRefresh) {
+        $scope.playlists = data.cards;
+      } else {
+        $scope.playlists = $scope.playlists.concat(data.cards);
+      }
+      total = data.total;
+      pagenum++;
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  }
+
+  $scope.moreDataCanBeLoaded = function() {
+    return $scope.playlists.length < total;
+  }
+  $scope.doRefresh = function() {
+    count = 10;
+    pagenum = 1;
+    total = 10;
+    $scope.loadWorkData(true);
+  }
+})
+
+.controller('WorkDetailCtrl', function($scope, $http, $rootScope, $stateParams) {
+
+  $http.get($rootScope.$host + '/application/japi/application/sealform/get/' + $stateParams.id).success(function(data) {
+    $scope.play = data;
+  });
+
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
