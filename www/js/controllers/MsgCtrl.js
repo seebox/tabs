100fdafc03c73e9790a3535b9c41fc4393527562
seebox <sxh1982@126.com>
2016-08-18 14:14:10 +0800

    shenxh

--- www/js/controllers/MsgCtrl.js
+++ www/js/controllers/MsgCtrl.js
@@ -1,10 +1,48 @@
 $controllers
 
-.controller('MsgCtrl', function($scope,$http,$rootScope) {
+  .controller('MsgCtrl', function($scope, $http, $rootScope) {
 
-   $http.get($rootScope.$host + "/lua-api/v1/proxy/unreads?source=msg&count=10&pagenum=1").success(function(data) {
-  		   $scope.playlists = data.unreads;
-   });
+  var count = 10,
+    pagenum = 1,
+    total = 10;
+  $scope.playlists = [];
+  $scope.loadData = function(isRefresh) {
+    $http.get($rootScope.$host + "/lua-api/v1/proxy/unreads", {
+      params: {
+        source: 'msg',
+        count: count,
+        pagenum: pagenum
+      }
+    }).success(function(data) {
+      if (isRefresh) {
+        $scope.playlists = data.unreads;
+      } else {
+        $scope.playlists = $scope.playlists.concat(data.unreads);
+      }
+      total = data.total;
+      pagenum++;
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+
+
+  }
+
+  $scope.moreDataCanBeLoaded = function() {
+    return $scope.playlists.length < total;
+  }
+  $scope.doRefresh = function() {
+    count = 10;
+    pagenum = 1;
+    total = 10;
+    $scope.loadData(true);
+  }
+
+  $scope.read = function(item) {
+    console.log(item);;
+  }
+
+
   var onOpenNotification = function(event) {
     alert(1);
   };
