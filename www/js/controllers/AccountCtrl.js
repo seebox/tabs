c41eb763edc6e93bcccc7b5386f13a0b97a060cb
unknown <18210@.(none)>
2016-08-19 17:15:35 +0800

    bin

--- www/js/controllers/AccountCtrl.js
+++ www/js/controllers/AccountCtrl.js
@@ -1,12 +1,45 @@
-$controllers
+$controllers.controller('AccountCtrl', function($scope, $http, $ionicModal, $rootScope) {
+	$scope.settings = {
+		enableFriends : true
+	};
+	$scope.logout = function() {
+		localStorage.removeItem("token");
+		localStorage.removeItem("acc");
+		window.location.href = "login.html";
+	};
+	
+	$http.get($rootScope.$host + '/lua-api/v1/user/profile').success(function(data){
+		$scope.profile=data;
+	});
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-  $scope.logout = function() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("acc");
-    window.location.href = "login.html";
-  }
+	$ionicModal.fromTemplateUrl('datum-modal.html', {
+		scope : $scope,
+		focusFirstInput:true
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
+	
+	$scope.profileObj={};
+
+	$scope.openModal = function(type,title) {
+		$scope.modal.show();
+		$scope.modalTitle=title;
+		$scope.profileType=type;
+		$scope.profileObj.mobile=$scope.profile.contact.mobile;
+		$scope.profileObj.officephone=$scope.profile.contact.officephone;
+	};
+
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+	};
+	
+	$scope.$on('$destroy', function() {
+		$scope.modal.remove();
+	});
+	
+	$scope.$on('modal.hidden', function() {
+	});
+
+	
+
 });
