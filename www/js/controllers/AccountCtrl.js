21c2bdba2aa0100005b0bdfe1aeca201e8d06238
unknown <18210@.(none)>
2016-08-22 15:16:23 +0800

    bin

--- www/js/controllers/AccountCtrl.js
+++ www/js/controllers/AccountCtrl.js
@@ -19,16 +19,39 @@ $controllers.controller('AccountCtrl', function($scope, $http, $ionicModal, $roo
 		$scope.modal = modal;
 	});
 	
-	$scope.profileObj={};
-
 	$scope.openModal = function(type,title) {
 		$scope.modal.show();
 		$scope.modalTitle=title;
 		$scope.profileType=type;
-		$scope.profileObj.mobile=$scope.profile.contact.mobile;
-		$scope.profileObj.officephone=$scope.profile.contact.officephone;
 	};
-
+	
+	$scope.submit=function(){
+		var json={
+			'mobile':$scope.profile.contact.mobile,
+			'officephone':$scope.profile.contact.officephone,
+			'sex':$scope.profile.base.sex?$scope.profile.base.sex:'',
+			'degree':$scope.profile.base.degree?$scope.profile.base.degree:''
+		};
+		$http({
+			method : 'post',
+			url : $rootScope.$host + '/together/japi/user/profile/modify?timer=' + new Date().getTime(),
+			data : json,
+			headers : {
+				'Content-Type' : 'application/x-www-form-urlencoded'
+			},
+			transformRequest : function(obj) {
+				var str = [];
+				for(var p in obj) {
+					str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+				}
+				return str.join("&");
+			}
+		}).success(function(data) {
+			$scope.modal.hide();
+		});
+		
+	};
+	
 	$scope.closeModal = function() {
 		$scope.modal.hide();
 	};
@@ -39,7 +62,27 @@ $controllers.controller('AccountCtrl', function($scope, $http, $ionicModal, $roo
 	
 	$scope.$on('modal.hidden', function() {
 	});
-
 	
 
-});
+})
+.directive('changeSelect', function() {
+	return {
+		restrict : 'A',
+		scope : false,
+		require : '?ngModel',
+		link : function(scope, element, attrs, ngModel) {
+			element.on('click','.item',function(e){
+				element.find('.ion-android-done').remove();
+				ngModel.$setViewValue($(this).attr('attr'));
+				$(this).append('<i class="ion-android-done positive f-r"></i>');
+			});
+			ngModel.$render=function(){
+				if(ngModel.$modelValue){
+					element.find('.item[attr='+ngModel.$modelValue+']').append('<i class="ion-android-done positive f-r"></i>');
+				}else{
+					ngModel.$setViewValue('');
+				}
+			};
+		}
+	};
+}); 
